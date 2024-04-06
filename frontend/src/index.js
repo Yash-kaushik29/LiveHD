54433c895feb5d2933e813cf7803e307d275b8f5
unknown <kaushikyash262@gmail.com>
2024-04-06 18:42:38 +0530

    Added google auth

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <GoogleOAuthProvider clientId="757088464941-egjg2j7vemu317c5j4c3s4rj4oo4iadc.apps.googleusercontent.com">
+        <App />
+      </GoogleOAuthProvider>
     </React.StrictMode>
   </BrowserRouter>
 );
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,17 +1,88 @@
-import React, { useState } from "react";
-import Header from "../components/Header";
+import React, { useEffect, useState } from "react";
 import meet from "../images/meet.jpeg";
 import { IoIosAddCircleOutline } from "react-icons/io";
 import { FaRegKeyboard } from "react-icons/fa6";
 import { MdOutlineVideoCall } from "react-icons/md";
 import { Link } from "react-router-dom";
+import { IoSettingsOutline } from "react-icons/io5";
+import { FaUserCircle } from "react-icons/fa";
+import { GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
 
 const Home = () => {
   const [meetId, setMeetId] = useState("");
+  const [show, setShow] = useState(false);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    if(localStorage.getItem("name")) {
+      setUser({
+        name: localStorage.getItem("name"),
+        email: localStorage.getItem('email'),
+      })
+      console.log(user);
+    }
+  }, [user])
+
+  const logout = () => {
+    localStorage.removeItem("name");
+    localStorage.removeItem("email");
+    setUser(null);
+  }
 
   return (
     <div>
-      <Header />
+      <div className="bg-slate-400 p-4">
+        <div className="w-full md:w-[80%] px-4 py-[10px] items-center flex justify-between mx-auto ">
+          <div className="text-3xl font-bold">LiveHD</div>
+          <ul className="flex text-black gap-5">
+            {/* //adding setting icon */}
+            <li>
+              <IoSettingsOutline className="text-3xl" />
+            </li>
+            {/* //adding user icon  */}
+            <li>
+              <FaUserCircle
+                className="text-3xl cursor-pointer"
+                onClick={() => setShow(!show)}
+              />
+            </li>
+          </ul>
+          {show && (
+            <div className="flex flex-col items-center justify-center py-2 w-[250px] absolute top-[70px] right-5 lg:right-[100px] bg-gray-100 rounded-xl shadow-lg">
+              <button
+                className="absolute top-2 right-4 font-bold"
+                onClick={() => setShow(!show)}
+              >
+                X
+              </button>
+              <FaUserCircle className="text-8xl py-2 text-gray-600" />
+              {user && <span className="font-semibold text-xl pb-2">{user.name}</span> }
+              {!user ? <GoogleLogin
+                onSuccess={(credentialResponse) => {
+                  const credentials = jwtDecode(JSON.stringify(credentialResponse));
+                  const email = credentials.email;
+                  const name = credentials.name;
+                  localStorage.setItem("name", name);
+                  localStorage.setItem("email", email);
+                  setUser({
+                    name,
+                    email
+                  })
+                }}
+                onError={() => {
+                  console.log("Login Failed");
+                }}
+              />
+              : <button className="bg-blue-500 text-white font-semibold px-8 py-2 rounded-3xl my-2" onClick={logout}>
+              Logout
+            </button>}
+              
+              
+            </div>
+          )}
+        </div>
+      </div>
       <div className="w-full h-screen flex justify-center items-center bg-slate-700">
         <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
           <img
